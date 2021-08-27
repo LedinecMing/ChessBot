2d564f2b16eeb448e90e9fd7da85b0e8419ae146
ermakovartem97 <ermakovartem97@mail.ru>
2021-08-27 20:36:01 +0300

    Добавил доску, сделал 1 ход, сохранил в SVG

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-import chess
 import chess.svg
 
-board = chess.Board()
+board = chess.Board()  # Создать доску
+print(board.legal_moves)  # Показать возможнжные ходы
 
-img = chess.svg.board(board, size=350)
+move = chess.Move.from_uci("g1f3")  # Сделать ход
+board.push(move)  # Отправить ход на доску
 
-print(img)
+img = chess.svg.board(board, lastmove=move, size=350)  # Создать SVG с последним ходом
+
+# записть SVG в файл для просмотра
+with open('board.svg', 'w') as f:
+    f.write(img)
